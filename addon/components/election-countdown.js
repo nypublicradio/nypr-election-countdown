15b7278b03ee34ab6c04e7a7f27e05f6a68add89
Kate Edmundson <kedmundson@nypublicradio.org>
2018-08-22 12:25:05 -0400

    TZ in times

--- addon/components/election-countdown.js
+++ addon/components/election-countdown.js
@@ -9,7 +9,7 @@ export default CountDown.extend({
   // put this in config/environment.js when import error gets resolved
   calendarLink: '/assets/midterms.ics',
 
-  electionDayEveStart: '2018-11-05T05:00:00.000Z',
-  electionDayStart: '2018-11-06T05:00:00.000Z',
-  electionDayEnd: '2018-11-07T05:00:00.000Z',
+  electionDayEveStart: '2018-11-05T00:00:00.000-05:00',
+  electionDayStart: '2018-11-06T00:00:00.000-05:00',
+  electionDayEnd: '2018-11-07T00:00:00.000-05:00'
 });
