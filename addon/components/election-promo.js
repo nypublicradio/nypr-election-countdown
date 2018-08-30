4f670e66dad965778b796906fbe604040f25c9bc
Michael Hearn <git@mikehearn.net>
2018-08-30 11:05:04 -0400

    Add cookie on exit

--- addon/components/election-promo.js
+++ addon/components/election-promo.js
@@ -1,26 +1,38 @@
-import Component from '@ember/component';
-import layout from '../templates/components/election-promo';
-import { inject as service } from '@ember/service';
+import Component from "@ember/component";
+import layout from "../templates/components/election-promo";
+import { inject as service } from "@ember/service";
+import { computed } from "@ember/object";
 
 export default Component.extend({
   layout,
   chunkContent: null,
-  chunkSlug: 'election-promo',
-  classNames: ['election-promo'],
+  chunkSlug: "election-promo",
+  classNames: ["election-promo"],
+  classNameBindings: ['alreadyShown:hidden'],
+
+  cookies: service(),
 
   init() {
     this._super(...arguments);
-    this.get('store').findRecord('chunk', this.chunkSlug).then((chunkContent) => {
-      this.set('chunkContent', chunkContent.get('content'));
-    })
+    this.get("store")
+      .findRecord("chunk", this.chunkSlug)
+      .then(chunkContent => {
+        this.set("chunkContent", chunkContent.get("content"));
+      });
   },
 
   store: service(),
 
-  countdownTo: '2018-11-06T00:00:00.000-05:00',
+  countdownTo: "2018-11-06T00:00:00.000-05:00",
+  alreadyShown: computed('cookies', function() {
+    let cookieService = this.get("cookies");
+    return cookieService.read("hasSeenElectionPromo");
+  }),
   actions: {
     closeModule() {
-      document.querySelector('.election-promo').remove();
+      let cookieService = this.get("cookies");
+      cookieService.write("hasSeenElectionPromo", true, {path: '/', domain: 'wnyc.org'});
+      document.querySelector(".election-promo").remove();
     }
   }
 });
