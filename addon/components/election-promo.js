655bc28b6b080f57f7271d29369c2ce7759e47e2
Brian Whitton <me@brianwhitton.com>
2018-08-30 12:31:27 -0400

    some clean up and code geography

--- addon/components/election-promo.js
+++ addon/components/election-promo.js
@@ -1,6 +1,6 @@
 import Component from "@ember/component";
 import layout from "../templates/components/election-promo";
-import { inject as service } from "@ember/service";
+import { inject } from "@ember/service";
 import { computed } from "@ember/object";
 
 export default Component.extend({
@@ -10,24 +10,25 @@ export default Component.extend({
   classNames: ["election-promo"],
   classNameBindings: ['alreadyShown:hidden'],
 
-  cookies: service(),
+  countdownTo: "2018-11-06T00:00:00.000-05:00",
+
+  store: inject(),
+  cookies: inject(),
 
   init() {
     this._super(...arguments);
-    this.get("store")
+    this.store
       .findRecord("chunk", this.chunkSlug)
       .then(chunkContent => {
-        this.set("chunkContent", chunkContent.get("content"));
+        this.set("chunkContent", chunkContent.content);
       });
   },
 
-  store: service(),
-
-  countdownTo: "2018-11-06T00:00:00.000-05:00",
   alreadyShown: computed('cookies', function() {
     let cookieService = this.get("cookies");
     return cookieService.read("hasSeenElectionPromo");
   }),
+
   actions: {
     closeModule() {
       let cookieService = this.get("cookies");
