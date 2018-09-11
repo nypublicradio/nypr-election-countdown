91cf1299a22b1bde34757ce72a5173d21cc58bc9
Kate Edmundson <kedmundson@nypublicradio.org>
2018-09-11 16:22:48 -0400

    add primary countdown states

--- addon/components/election-countdown.js
+++ addon/components/election-countdown.js
@@ -9,7 +9,16 @@ export default CountDown.extend({
   // put this in config/environment.js when import error gets resolved
   calendarLink: '/assets/midterms.ics',
 
+  diffUntilPrimary() {
+    this.set('to', this.primaryDayStart);
+    return this.diff();
+  },
+
   electionDayEveStart: '2018-11-05T00:00:00.000-05:00',
   electionDayStart: '2018-11-06T00:00:00.000-05:00',
   electionDayEnd: '2018-11-07T00:00:00.000-05:00'
+
+  primaryDayEveStart: '2018-09-12T00:00:00.000-05:00',
+  primaryDayStart: '2018-09-13T00:00:00.000-05:00',
+  primaryDayEnd: '2018-09-14T00:00:00.000-05:00'
 });
